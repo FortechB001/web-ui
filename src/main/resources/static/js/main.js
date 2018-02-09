6e5a5a76ff136944f10df3e8cc124822bdc93326
Marius <aniov_usa@yahoo.com>
2018-02-09 14:01:38 +0200

    almost done

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,9 @@
 let products = [];
 let productId;
 let howManySelected;
+let selectedProduct = "";
+let daysToArriveInDeposit = -1;
+let order;
 
 
 window.onload = function () {
@@ -34,6 +37,8 @@ function populateProductsList() {
     for (let i = 0; i < products.length; i++) {
         list[i] =
             $('<a>', {
+                href: '#',
+                id: products[i].id,
                 class: 'list-group-item',
                 text: products[i].name,
                 onclick: "selectProduct('" + products[i].id + "')",
@@ -44,6 +49,14 @@ function populateProductsList() {
 
 function selectProduct(prId) {
 
+    if (selectedProduct !== "") {
+        document.getElementById(selectedProduct).className = "list-group-item";
+    }
+    selectedProduct = prId;
+    document.getElementById(prId).className = "list-group-item active";
+    hideDeliveryEstimationCard();
+    hideConfirmDeliveryCard();
+    hideOrderCard();
     productId = prId;
 
     //stock-service
@@ -66,14 +79,14 @@ function showStockResults(stock) {
     document.getElementById('howMany').innerHTML = "";
     if (stock.stockTotal !== 0) {
         document.getElementById("howManyContinueButton").className = "btn btn-primary enabled";
-        createDropDownList(stock.stockTotal);
+        createHowManyDropDownList(stock.stockTotal);
     } else {
         document.getElementById("howManyContinueButton").className = "btn btn-primary disabled";
     }
     showStockCard();
 }
 
-function createDropDownList(howMany) {
+function createHowManyDropDownList(howMany) {
 
     let list = [];
     for (let i = 0; i < howMany; i++) {
@@ -88,9 +101,9 @@ function createDropDownList(howMany) {
 document.getElementById('howManyContinueButton').onclick = function () {
     let selected = document.getElementById('howMany');
     let howMany = selected.selectedIndex + 1;
+    howManySelected = howMany;
 
     getDeliveryTimeToDeposit(howMany);
-
     console.log("howMany: " + howMany);
 };
 
@@ -102,6 +115,7 @@ function getDeliveryTimeToDeposit(howMany) {
         type: "GET",
         contentType: "application/json; charset=utf-8",
         success: function (data, textStatus, jqXHR) {
+            daysToArriveInDeposit = data;
             showDeliveryEstimation(data);
         },
         error: function (data, textStatus, jqXHR) {
@@ -111,8 +125,148 @@ function getDeliveryTimeToDeposit(howMany) {
 }
 
 function showDeliveryEstimation(daysToArriveInDeposit) {
-    console.log("daysToArriveInDeposit: " + daysToArriveInDeposit);
+
+    document.getElementById("countryContinueButton").className = "btn btn-primary disabled";
+    showDeliveryEstimationResult(daysToArriveInDeposit);
+    populateDeliveryCountryList();
+    showDeliveryEstimationCard();
 }
+
+function showDeliveryEstimationResult(daysToArriveInDeposit) {
+    document.getElementById('daysInOurStock').innerHTML = daysToArriveInDeposit;
+}
+
+function populateDeliveryCountryList() {
+
+    //estimation-service
+    $.ajax({
+        url: "http://localhost:8085/estimation/countries",
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        success: function (data, textStatus, jqXHR) {
+            createCountriesDropDownList(data);
+        },
+        error: function (data, textStatus, jqXHR) {
+            console.log("Cannot read countries list");
+        }
+    });
+}
+
+function createCountriesDropDownList(countries) {
+
+    if (countries.length === 0) {
+        return;
+    }
+    document.getElementById("countryContinueButton").className = "btn btn-primary enabled";
+    document.getElementById('yourCountry').innerHTML = "";
+    let list = [];
+    for (let i = 0; i < countries.length; i++) {
+        list[i] =
+            $('<option>', {
+                text: countries[i]
+            })
+    }
+    $(document.getElementById('yourCountry')).append(list);
+}
+
+document.getElementById('countryContinueButton').onclick = function () {
+    let selected = document.getElementById('yourCountry');
+    let country = selected.options[selected.selectedIndex].value;
+
+    getConfirmDeliveryToUser(country);
+    console.log("selected country: " + country);
+};
+
+function getConfirmDeliveryToUser(country) {
+    //estimation-service
+    let url = "http://localhost:8085/estimation/delivery?" +
+        "id=" + productId + "&howMany=" + howManySelected + "&daysToArriveInDeposit=" +
+        daysToArriveInDeposit + "&country=" + country;
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        success: function (data, textStatus, jqXHR) {
+            order = data;
+            showConfirmDelivery();
+        },
+        error: function (data, textStatus, jqXHR) {
+            console.log("Cannot read delivery to user estimation");
+        }
+    });
+}
+
+document.getElementById('confirmDeliveryButton').onclick = function () {
+    showOrderCard();
+    showOrderDetails();
+};
+
+function showOrderDetails() {
+    document.getElementById('orderProductName').innerHTML = order.productId;
+    document.getElementById('orderQuantity').innerHTML = order.howMany;
+    document.getElementById('orderDaysToArrive').innerHTML = order.daysToCustomer;
+}
+
+function showConfirmDelivery() {
+    showConfirmDeliveryResult();
+    showConfirmDeliveryCard();
+}
+
+function showConfirmDeliveryResult() {
+    document.getElementById('daysToCustomer').innerHTML = order.daysToCustomer;
+}
+
+document.getElementById('orderProductButton').onclick = function () {
+
+    let result;
+
+    //order-service
+    let url = "http://localhost:8086/order/make?" +
+        "id=" + productId +
+        "&howMany=" + howManySelected +
+        "&daysToArriveInDeposit=" + daysToArriveInDeposit +
+        "&country=" + order.country +
+        "&daysToCustomer=" + order.daysToCustomer;
+
+    $.ajax({
+        url: url,
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        success: function (data, textStatus, jqXHR) {
+            console.log("ORDER SUCCESS: " + textStatus + " " + jqXHR);
+        },
+        error: function (xhr, status) {
+            console.log("Cannot make the order");
+        },
+        complete: function (jqXHR, status) {
+            showModal(jqXHR.responseText);
+        }
+    });
+};
+
+function showModal(result) {
+    document.getElementById('orderMessage').innerHTML = result;
+    $('#myModal').modal('show');
+
+    hideDeliveryEstimationCard();
+    hideConfirmDeliveryCard();
+    hideOrderCard();
+    hideStockCard();
+    document.getElementById(selectedProduct).className = "list-group-item";
+}
+
+
+$('#howMany').change(function () {
+    hideDeliveryEstimationCard();
+    hideConfirmDeliveryCard();
+    hideOrderCard();
+});
+
+$('#yourCountry').change(function () {
+    hideConfirmDeliveryCard();
+    hideOrderCard();
+});
 
 function showStockCard() {
     $('#detailsCard').show();
@@ -120,4 +274,28 @@ function showStockCard() {
 
 function hideStockCard() {
     $('#detailsCard').hide();
+}
+
+function showDeliveryEstimationCard() {
+    $('#estimationCard').show();
+}
+
+function hideDeliveryEstimationCard() {
+    $('#estimationCard').hide();
+}
+
+function showConfirmDeliveryCard() {
+    $('#confirmDelivery').show();
+}
+
+function hideConfirmDeliveryCard() {
+    $('#confirmDelivery').hide();
+}
+
+function showOrderCard() {
+    $('#orderProduct').show();
+}
+
+function hideOrderCard() {
+    $('#orderProduct').hide();
 }
